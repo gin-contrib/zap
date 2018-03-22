26d5a72cacce2c0f12082a494ca2132c3ed40949
erdnaxeli <erdnaxeli@gmail.com>
2018-03-22 13:47:36 +0100

    feat: log query (#6)

--- zap.go
+++ zap.go
@@ -22,6 +22,7 @@ func Ginzap(logger *zap.Logger, timeFormat string, utc bool) gin.HandlerFunc {
 		start := time.Now()
 		// some evil middlewares modify this values
 		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
 		c.Next()
 
 		end := time.Now()
@@ -40,6 +41,7 @@ func Ginzap(logger *zap.Logger, timeFormat string, utc bool) gin.HandlerFunc {
 				zap.Int("status", c.Writer.Status()),
 				zap.String("method", c.Request.Method),
 				zap.String("path", path),
+				zap.String("query", query),
 				zap.String("ip", c.ClientIP()),
 				zap.String("user-agent", c.Request.UserAgent()),
 				zap.String("time", end.Format(timeFormat)),
