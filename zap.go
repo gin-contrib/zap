de3bf1063d79bdb8fca6995878b2d16edf33af33
ota42y <ota42y+github@gmail.com>
2021-08-31 09:24:13 +0900

    Support skip logging for specific path (#14)

--- zap.go
+++ zap.go
@@ -15,6 +15,13 @@ import (
 	"go.uber.org/zap"
 )
 
+// Config is config setting for Ginzap
+type Config struct {
+	TimeFormat string
+	UTC        bool
+	SkipPaths  []string
+}
+
 // Ginzap returns a gin.HandlerFunc (middleware) that logs requests using uber-go/zap.
 //
 // Requests with errors are logged using zap.Error().
@@ -24,6 +31,16 @@ import (
 //   1. A time package format string (e.g. time.RFC3339).
 //   2. A boolean stating whether to use UTC time zone or local.
 func Ginzap(logger *zap.Logger, timeFormat string, utc bool) gin.HandlerFunc {
+	return GinzapWithConfig(logger, &Config{TimeFormat: timeFormat, UTC: utc})
+}
+
+// GinzapWithConfig returns a gin.HandlerFunc using configs
+func GinzapWithConfig(logger *zap.Logger, conf *Config) gin.HandlerFunc {
+	skipPaths := make(map[string]bool, len(conf.SkipPaths))
+	for _, path := range conf.SkipPaths {
+		skipPaths[path] = true
+	}
+
 	return func(c *gin.Context) {
 		start := time.Now()
 		// some evil middlewares modify this values
@@ -31,28 +48,30 @@ func Ginzap(logger *zap.Logger, timeFormat string, utc bool) gin.HandlerFunc {
 		query := c.Request.URL.RawQuery
 		c.Next()
 
-		end := time.Now()
-		latency := end.Sub(start)
-		if utc {
-			end = end.UTC()
-		}
-
-		if len(c.Errors) > 0 {
-			// Append error field if this is an erroneous request.
-			for _, e := range c.Errors.Errors() {
-				logger.Error(e)
+		if _, ok := skipPaths[path]; !ok {
+			end := time.Now()
+			latency := end.Sub(start)
+			if conf.UTC {
+				end = end.UTC()
 			}
-		} else {
-			logger.Info(path,
-				zap.Int("status", c.Writer.Status()),
-				zap.String("method", c.Request.Method),
-				zap.String("path", path),
-				zap.String("query", query),
-				zap.String("ip", c.ClientIP()),
-				zap.String("user-agent", c.Request.UserAgent()),
-				zap.String("time", end.Format(timeFormat)),
-				zap.Duration("latency", latency),
-			)
+
+			if len(c.Errors) > 0 {
+				// Append error field if this is an erroneous request.
+				for _, e := range c.Errors.Errors() {
+					logger.Error(e)
+				}
+			} else {
+				logger.Info(path,
+					zap.Int("status", c.Writer.Status()),
+					zap.String("method", c.Request.Method),
+					zap.String("path", path),
+					zap.String("query", query),
+					zap.String("ip", c.ClientIP()),
+					zap.String("user-agent", c.Request.UserAgent()),
+					zap.String("time", end.Format(conf.TimeFormat)),
+					zap.Duration("latency", latency),
+				)
+			}
 		}
 	}
 }
